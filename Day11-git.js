51d647dfd1d1ee92c6b10fa3410fd5d3aa993b63
KumaravelS413 <kumaravels413@gmail.com>
2026-03-18 14:35:36 +0530

    Adddata

---
+++ Day11-git.js
@@ -0,0 +1,9 @@
+// SLICE -- based on starting index between ending index +1
+
+var str = "javascript programming";
+
+var store = str.slice(0, 10); // 10+1
+console.log(store)
+
+var store = str.slice(11, 22);
+console.log(store)
